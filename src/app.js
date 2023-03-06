4f9e2aefe8c4f1ec7e57be596702c501ebb87318
nhtony <honhathao062@gmail.com>
2023-03-06 21:50:53 +0700

    Implement sign up

--- src/app.js
+++ src/app.js
@@ -9,17 +9,19 @@ const app = express();
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // init db
 require("./dbs/init.mongodb");
 // const { checkOverload } = require("./helpers/check.connect");
 // checkOverload()
 // init routes
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Welcome Javascript !",
-  });
-});
+app.use("", require("./routes"));
 
 // handling errors
 
---
+++ src/services/access.service.js
@@ -0,0 +1,103 @@
+const bycrypt = require("bcrypt");
+const crypto = require("node:crypto");
+const KeyTokenService = require("./keyToken.service");
+const shopModel = require("../models/shop.model");
+const { createTokenPair } = require("../auth/authUtil");
+const { getInfoData } = require("../utils");
+
+const RoleShop = {
+  SHOP: "SHOP",
+  WRITER: "WRITER",
+  EDITER: "EDITER",
+  ADMIN: "ADMIN",
+};
+
+class AccessService {
+  static signUp = async ({ name, email, password }) => {
+    try {
+      // step1: check email exists ???
+      const holderShop = await shopModel.findOne({ email }).lean();
+      if (holderShop) {
+        return {
+          code: "zzzz",
+          message: "Shop already registered",
+        };
+      }
+
+      const passwordHash = await bycrypt.hash(password, 10);
+      const newShop = await shopModel.create({
+        name,
+        email,
+        password: passwordHash,
+        roles: [RoleShop.SHOPsxw],
+      });
+
+      if (newShop) {
+        // created privateKey, publicKey
+        // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+        //   modulusLength: 4096,
+        //   publicKeyEncoding: {
+        //     type: "pkcs1",
+        //     format: "pem",
+        //   },
+        //   privateKeyEncoding: {
+        //     type: "pkcs1",
+        //     format: "pem",
+        //   },
+        // });
+
+        const publicKey = crypto.randomBytes(64).toString("hex");
+        const privateKey = crypto.randomBytes(64).toString("hex");
+
+        console.log("newShop", newShop);
+        console.log({ privateKey, publicKey }); // save collection KeyStore
+
+        const keyStore = await KeyTokenService.createKeyToken({
+          userId: newShop._id,
+          publicKey,
+          privateKey,
+        });
+
+        if (!keyStore) {
+          return {
+            code: "xxx",
+            message: "keyStore error",
+          };
+        }
+
+        // created token pair
+        const tokens = await createTokenPair(
+          { userId: newShop._id, email },
+          publicKey,
+          privateKey
+        );
+
+        console.log(`Created Token Success::`, tokens);
+
+        return {
+          code: 201,
+          metadata: {
+            shop: getInfoData({
+              fileds: ["_id", "name", "email"],
+              object: newShop,
+            }),
+            tokens,
+          },
+        };
+      }
+
+      return {
+        code: 200,
+        metadata: null,
+      };
+    } catch (error) {
+      return {
+        code: "zzz",
+        message: error.message,
+        status: "error",
+      };
+    }
+  };
+}
+
+module.exports = AccessService;
